66cbc716144e7e4adac25f568b46fa1f78ba485c
mdaquin <mathieu@daquin.net>
2024-08-20 11:56:28 +0000

    new process to create soms based on ksom

--- process_model.py
+++ process_model.py
@@ -1,10 +1,9 @@
 import pandas as panda
 import torch
-import sys
+import sys, os
 import json
-
+from ksom import SOM
 ## NEXT STEPS:: ##
-# Way to go through each layer
 # Create dataset as csv
 # Run training sets
 # Get activations of each layer... if conv2D, concat matrix of each channel separetly
@@ -14,7 +13,8 @@ def load_model(fn, device="cpu"):
     return torch.load(fn).to(device)
 
 def set_up_activations(model):
-    global activation 
+    global activation
+    llayers = []
     def get_activation(name):
         def hook(model, input, output):
             if type(output) != torch.Tensor: activation[name] = output
@@ -26,8 +26,11 @@ def set_up_activations(model):
             nmo = getattr(mo,k)
             nmo.register_forward_hook(get_activation(name))
             print("--"+"--"*lev, "hook added for",name)
+            llayers.append(name)
             rec_reg_hook(nmo, prev=name, lev=lev+1)
-    rec_reg_hook(model)
+        return llayers
+    return rec_reg_hook(model)
+    
 
 if __name__ == "__main__":
 
@@ -42,4 +45,28 @@ if __name__ == "__main__":
     exec(open(config["modelclass"]).read())
     model = load_model(config["model"], device=device)
     activation = {}
-    set_up_activations(model)
+    list_layers = set_up_activations(model)
+
+    som_size = config["som_size"]
+    base_som_dir = config["base_som_dir"]
+    
+    SOMs = {}
+
+    data_dir = config["dataset_dir"]
+    for f in os.listdir(data_dir):
+        print("*** loading", f, "***")
+        data = json.load(open(data_dir+"/"+f))
+        IS = []
+        for d in data:
+            IS.append(d["I"])
+        IS = torch.Tensor(IS).to(device)
+        print("*** applying model ***")
+        activation = {}
+        P = model(IS)
+        for layer in activation:
+            acts = torch.flatten(activation[layer], start_dim=1)
+            if layer not in SOMs: SOMs[layer] = SOM(som_size[0], som_size[1], acts.shape[1], neighborhood_init=som_size[0], neighborhood_drate=0.001)
+            print("   *** adding to SOM for",layer)
+            SOMs[layer].add(acts)
+            torch.save(SOMs[layer], base_som_dir+"/"+layer+".pt")
+        print("*** done ***")
