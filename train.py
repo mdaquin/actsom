172df429a24eb08446055ec4f6b96c02aa2d502c
mdaquin <mathieu.daquin.w+gh@gmail.com>
2025-08-07 13:42:05 +0200

    nex training script that takes activation files directly.

--- train.py
+++ train.py
@@ -1,16 +1,96 @@
 import torch, sys, json
+sys.path.insert(0, "../KSOM/src/ksom")
 from ksom import SOM, cosine_distance, euclidean_distance, nb_gaussian, nb_linear, nb_ricker
 from torch.utils.data import DataLoader
 from tqdm import tqdm
+import pandas as pd
+from matplotlib import pyplot as plt
+import pygame
+from sklearn.decomposition import PCA
+
+def display(map, xoffset=0):
+    if map.shape[1] > 3:
+        pca = PCA(n_components=3)
+        somap = pca.fit_transform(map.detach())
+    else: somap = map
+    for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+    unit = int(screen_size/som_size[0])
+    for i,cs in enumerate(somap):
+        x = int(i/som_size[0])
+        y = i%som_size[0]
+        x = (x*unit)+xoffset
+        y = y*unit
+        try : 
+            color = (max(min(255, int(cs[0]*255)), 0),
+                     max(min(255, int(cs[1]*255)), 0),
+                     max(min(255, int(cs[2]*255)), 0))
+        except: 
+            print(cs*255)
+            sys.exit(-1)
+        pygame.draw.rect(surface,
+                         color,
+                         pygame.Rect(x, y, unit, unit))
+        # if labels is not None:
+        #      lab = findLabel(i, map, labels)
+        #      cp = surface.get_at((int(x+label_offset+unit/20),y+int(unit/5)))
+        #      cl = (200, 200, 200)
+        #      if cp[0] > 100 : cl = (0, 0, 0)
+        #      texts = font.render(lab, False, cl)
+        #      surface.blit(texts, (x+label_offset+unit/20,y+int(unit/5)))
+
+    pygame.display.flip()
+    pygame.display.update()
+
+
+def train_som(dataloader, training_log):
+    with torch.no_grad():
+            som = None
+            for ep in range(1, nepochs+1):
+                print("*** Epoch",ep,"***")
+                freqmap = torch.zeros(som_size[0]*som_size[1])
+                pbar = tqdm(dataloader, f"Training SOM for {layer}")
+                for acts in pbar:
+                    acts=acts.to(device)
+                    if som is None:
+                        sample = acts[:som_size[0]*som_size[1]]
+                        mins = acts.min(dim=0).values.to(device)
+                        maxs = acts.max(dim=0).values.to(device)
+                        som = SOM(som_size[0], som_size[1], len(activations[layer][0]), 
+                                  dist=distance,
+                                  neighborhood_init=som_size[0]*1.0, 
+                                  neighborhood_drate=0.00001*som_size[0], 
+                                  zero_init=True, sample_init=sample,
+                                  minval=mins, maxval=maxs, 
+                                  alpha_init=alpha, alpha_drate=alpha_drate, 
+                                  neighborhood_fct=neighb_func, 
+                                  device=device, return_dist=True)
+                    # normalisation
+                    acts = ((acts-mins)/(maxs-mins))
+                    change,count, dist = som.add(acts)
+                    pbar.set_postfix({'ch': f'{change:.8f}', 'd': f'{dist:.8f}'})
+                    training_log.append({"layer": layer, "epoch": ep, "count": count, "change": change, "distance": dist})
+                    bmu, dists = som(acts)
+                    for i in bmu: freqmap[i[0]*som_size[0]+i[1]] += 1
+                    ffreqmap = (freqmap - freqmap.min())/(freqmap.max()-freqmap.min())
+                    # freqmap = freqmap.view(len(freqmap), 1).repeat((1,3))
+                    display(ffreqmap.view(len(freqmap), 1).repeat((1,3)), xoffset=screen_size)
+                    display(som.somap.cpu())
+                    # saving som
+                    torch.save(som, f"{somdir}/{layer}_{som_size[0]}x{som_size[1]}.pth")
 
 if __name__ == "__main__":
-
     if len(sys.argv) != 2:
         print("provide configuration file (JSON)")
         sys.exit(1)
     conf = sys.argv[1]
 
     # base config
+    screen_size = 600
+    pygame.init()
+    surface = pygame.display.set_mode((screen_size*2,screen_size))
     config = json.load(open(conf))
     if "seed" in config: torch.manual_seed(config["seed"])
     if "som_size" in config: som_size = config["som_size"]
@@ -43,7 +123,7 @@ if __name__ == "__main__":
         print("Unknown neighb_func", neighb_func)
         sys.exit(1)
     if "activationfile" in config: 
-        data = torch.load(config["activationfile"])
+        data = torch.load(config["activationfile"], weights_only=False)
         if "activation_field" in config: 
             if config["activation_field"] in data: activations = data[config["activation_field"]]
             else:
@@ -54,108 +134,22 @@ if __name__ == "__main__":
     else: 
         print("activationfile or activationcode required in config (only activationfile implemented)")
         sys.exit(1)
-    base_som_dir = config["somdir"]
+    if "somdir" in config: somdir = config["somdir"]
+    else: 
+        print("somdir required in configuration")
+        sys.exit(1)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     runcpu = "runcpu" in config and config["runcpu"]
     if runcpu: device = torch.device("cpu")
     if device == torch.device("cuda"): print("USING GPU")
-       
+    training_log = []
     for layer in activations: # make a train_som function and a train 1 epoch function
-        with torch.no_grad(): # basic som training does not require gradient
-            dataloader = DataLoader(activations[layer], batch_size=batch_size, shuffle=True)
-            print()
-            som = None
-            for ep in range(1, nepochs+1):
-                for acts in tqdm(dataloader, f"Training SOM for {layer}"):
-                    if som is None:
-                        print(acts)
-                        sample = acts[:som_size[0]*som_size[1]]
-                        mins = acts.min(dim=0).values.to(device)
-                        maxs = acts.max(dim=0).values.to(device)
-                        som = SOM(som_size[0], som_size[1], len(activations[layer][0]), 
-                                  dist=distance,
-                                  neighborhood_init=som_size[0]*1.0, 
-                                  neighborhood_drate=0.00001*som_size[0], 
-                                  zero_init=True, sample_init=sample,
-                                  minval=mins, maxval=maxs, 
-                                  alpha_init=alpha, alpha_drate=alpha_drate, 
-                                  neighborhood_fct=neighb_func, 
-                                  device=device)
+         dataloader = DataLoader(activations[layer], batch_size=batch_size, shuffle=True)
+         train_som(dataloader, training_log)   
+       
+    # saving logs
+    df = pd.DataFrame(training_log)
+    df.to_csv("training_log.csv")
 
-    SOMs = {}
-    mm = {}
-    with torch.no_grad(): # SOM training does not require gradients
-        count=0
-        sev  = 0  
-        for X, y in data_loader:
-            if not runcpu: X = X.to(device)
-            if not runcpu: y = y.to(device)
-            # print("   ** applying model")
-            u.activation = {}
-            p = model(X)
-            if config["eval"] == "precision":
-                # multiclass
-                if len(y.shape) > 1:
-                    y = torch.argmax(y, dim=1)
-                    p = torch.argmax(p, dim=1)
-                    sev += (y==p).sum()/len(p)
-                else: sev += 1-(abs(y - (p>=0.5).to(torch.int).T[0]).sum()/len(p))
-            elif config["eval"] == "mae":
-                sev += abs(y - p.T[0]).sum()/len(p) 
-            elif config["eval"] == "mse":
-                sev += ((y - p.T[0])**2).sum()/len(p) 
-            count+=1
-            for layer in u.activation:
-                # in case of LSTM, it is a tuple
-                # output is the first element
-                if type(u.activation[layer]) == tuple: u.activation[layer] = u.activation[layer][0]
-                # dealing with MegNet weird activation shapes
-                if len(u.activation[layer].shape) < 2: continue
-                if not u.activation[layer].shape[0] == config["batchsize"]:
-                    if u.activation[layer].shape[1] == config["batchsize"]:
-                        u.activation[layer] = u.activation[layer].T
-                    else: continue
-                if config["aggregation"] == "flatten": acts = torch.flatten(u.activation[layer], start_dim=1).to(device)
-                elif config["aggregation"] == "mean":
-                    if len(u.activation[layer].shape) > 2:
-                        acts = torch.mean(u.activation[layer], dim=1).to(device)
-                    else: acts = u.activation[layer].to(device)
-                else: 
-                    print("unknown aggregation, check config")
-                    sys.exit(-1)
-                if acts.shape[1] < 10: continue
-               
-                   
-                # normalisation based on min/max of first dataset
-                if acts.shape[1] != mm[layer]["min"].shape[0] or acts.shape[1] != mm[layer]["max"].shape[0]: 
-                    #if layer in SOMs: 
-                        #print("*** dropping SOM", layer)
-                        #del SOMs[layer]
-                    continue                
-                print("*** progressing with", layer)
-                acts = (acts-mm[layer]["min"])/(mm[layer]["max"]-mm[layer]["min"])
-                if layer not in SOMs and len(acts.shape) == 2: # how can it not be?
-                  print("   ** creating", layer)
-                  perm = torch.randperm(acts.size(0))
-                  samples = acts[perm[-(som_size[0]*som_size[1]):]]
-
-
-
-                  SOMevs[layer] = {"change": 0.0, "count": 0}
-                if layer not in SOMs: continue
-                change,count2 = SOMs[layer].add(acts.to(device))
-                SOMevs[layer]["change"] += change
-                SOMevs[layer]["count"] += count2
-                # NaNs happen quickly, from first relu layer.
-                # this is a trick... should be investigated why we get NaNs in the SOM
-                if torch.isnan(SOMs[layer].somap).any(): 
-                    print ("*** NaN!")
-                    SOMs[layer].somap = torch.nan_to_num(SOMs[layer].somap, 0.0)   
-        print(f"{ep}:: Model eval={sev/count}, mem use: {torch.cuda.memory_allocated('cuda:0')/(1014**3):.2f}GB")
-        for layer in SOMevs:
-            SOMevs[layer]["change"] /= count
-            SOMevs[layer]["count"] /= count
-            print(f"    {layer}:: change={SOMevs[layer]['change']}, count={SOMevs[layer]['count']}")
-            # save SOMs     
-            torch.save(SOMs[layer], base_som_dir+"/"+layer+".pt")
-    
+    df[["change", "distance"]].plot()
+    plt.show()
